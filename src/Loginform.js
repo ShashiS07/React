afb139a1639a7bfa041503ae9cb46540b9b129de
ShashiS07 <shashiss0708@gmail.com>
2023-01-05 16:59:24 +0530

    done

--- src/Loginform.js
+++ src/Loginform.js
@@ -1,97 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import './Loginform.css'
 
-class Loginform extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            "First Name":"",
-            "Last Name":"",
-            "email":"",
-            "country":"India"
-        }
+function Loginform() {
 
-    }
- 
-    handleFirstNameChange=(value)=> {
-        this.setState({
-            "First Name":value,
-        })
-    }
-    handleLastNameChange=(value)=> {
-        this.setState({
-            "Last Name":value,
-        })
-    }
-    handleEmail=(value)=>{
-        this.setState({
-            "email":value
-        })
-    }
-    handleCountry=(value)=>{
-        this.setState({
-            "country":value
-        })
-    }
-    handleGender=(value)=>{
-        this.setState({
-            "gender":value
-        })
-    }
-    handleCheckbox=(value)=>{
-        this.setState({
-            "checkbox":value
-        })
-    }
-    handleImage=(value)=>{
-        this.setState({
-            "Image":value
-        })
-    }
-    handleFormSubmit(event){
-        event.preventDefault();
-        console.log("Form Submitted")
-    }
-    handleFormReset(event){
-        event.preventDefault();
-        console.log("Form Reset")
-    }
-    render(){
-        return <div className="formData">
-  
-            <form onSubmit={(event)=>this.handleFormSubmit(event)} onReset={(event)=>this.handleFormReset(event)}>
+    const [firstName,setFirstName]=useState("")
+    const [lastName,setLastName]=useState("")
+    const [email,setEmail]=useState("")
+    const [checkbox,setCheckbox]=useState(false)
+    const [gender,setGender]=useState("Male")
+    const [country,setCountry]=useState()
+    const [image,setImage]=useState("")
+
+
+
+
+    return <div className="formData">
+        <form onSubmit={(event) => {event.preventDefault()}} 
+        onReset={(event) => event.preventDefault()}
+        >
             <label>First Name:</label>
-            <input type="text" placeholder="Enter first Name" value={this.state["First Name"]} onChange={
-                (event)=>this.handleFirstNameChange(event.target.value)
-            }/> <br/>
+            <input type="text" placeholder="Enter first Name" value={firstName} onChange={
+                (event) => setFirstName(event.target.value)
+            } /> <br />
             <label>Last Name:</label>
-            <input type="text" placeholder="Enter last Name" value={this.state["Last Name"]} onChange={
-                (event)=>this.handleLastNameChange(event.target.value)
-            }/>
+            <input type="text" placeholder="Enter last Name" value={lastName} onChange={
+                (event) => setLastName(event.target.value)
+            } />
             <div>
                 <label>Email:</label>
-                <input type="email" placeholder="Enter email" value={this.state.email} onChange={
-                    (event)=>this.handleEmail(event.target.value)
-                }/>
+                <input type="email" placeholder="Enter email" value={email} onChange={
+                    (event) => setEmail(event.target.value)
+                } />
             </div>
             <div>
-                <label>Do you want to Recieve Marketing Mails? </label> <br/>
-                <input type="checkbox" name="check" value="Yes" defaultChecked onChange={(event)=>this.handleCheckbox(event.target.value)}/>Yes
-                <input type="checkbox" name="check" value="No" onChange={(event)=>this.handleCheckbox(event.target.value)}/>No
+                <label>Do you want to Recieve Marketing Mails? </label> <br />
+                <input type="checkbox" name="check" value={checkbox} onChange={(event) => setCheckbox(event.target.value)} />Yes
             </div>
             <div>
                 <label>Gender:</label>
-                <input type="radio" name="Gender" value="Male" defaultChecked 
-                onChange={(event)=>this.handleGender(event.target.value)}/>Male
-                <input type="radio" name="Gender" value="Female" 
-                onChange={(event)=>this.handleGender(event.target.value)}/>Female
-                <input type="radio" name="Other" value="Female" 
-                onChange={(event)=>this.handleGender(event.target.value)}/>Other
+                <input type="radio" name="Gender" value={gender}
+                    onChange={(event) => setGender(event.target.value)} />Male
+                <input type="radio" name="Gender" value={gender}
+                    onChange={(event) => setGender(event.target.value)} />Female
+                <input type="radio" name="Gender" value={gender}
+                    onChange={(event) => setGender(event.target.value)} />Other
             </div>
             <div>
                 <label>Country:</label>
-                <select value={this.state.country} onChange={
-                    (event)=>this.handleCountry(event.target.value)
+                <select value={country} onChange={
+                    (event) => setCountry(event.target.value)
                 }>
                     <option value="India">India</option>
                     <option value="Sri Lanka">Sri Lanka</option>
@@ -102,15 +59,23 @@ class Loginform extends React.Component{
             </div>
             <div>
                 <label>Profile Picture: </label>
-                <input type="file" onChange={(event)=>this.handleImage(event.target.value)} />
+                <input type="file" value={image} onChange={(event) => setImage(event.target.value)} />
             </div>
             <div>
                 <button type="submit">Submit</button>
                 <button type="reset">Reset</button>
             </div>
-            </form>
+        </form>
+        {firstName && lastName && email  ? (
+        <div style={{ marginTop: '10px' }}>
+          Things you have filled up: <br />
+          First Name: {firstName} <br />
+          Last Name: {lastName} <br />
+          Email: {email}
         </div>
-    }
+      ) : null}
+    </div>
 }
+
 
 export default Loginform
